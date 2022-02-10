1c468c28bba13e0bfca398a5e48834756f16e612
itsuki-jp <itsuki19601@gmail.com>
2022-02-10 10:21:25 +0900

    画面にたくさん表示

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let canvas = document.querySelector('#canvas');
 let ctx = canvas.getContext('2d');
 const W = canvas.width;
 const H = canvas.height;
-const size_W = 100;
-const size_H = 100;
+const size_W = 50;
+const size_H = 50;
 const shooter = new Array(1);
 const enemy = new Array(3);
+let enemyData = new Array(5 * 5); // { x: x, h: h, exist: true };
+let shooterData = { x: W - 250, h: H - size_H, shooting: false };
 
 const init = () => {
     ctx.beginPath();
@@ -27,11 +29,18 @@ const init = () => {
     loadImage(enemy, "enemy")
 }
 const initGame = () => {
+    ctx.drawImage(shooter[0], W - 250, H - size_H, size_W, size_H);
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5; j++) {
+            ctx.drawImage(enemy[i], 2 * size_W + size_W * j, size_H * i, size_W, size_H);
+            enemyData[4 * i + j] = [2 * size_W + size_W * j, size_H * i, i];
+            enemyData[4 * i + j] = { x: 2 * size_W + size_W * j, h: size_H * i, exist: true };
+        }
+    }
+    mainGame();
+}
+const mainGame = () => {
 
-    ctx.drawImage(shooter[0], W - 250, H - 100, size_W, size_H);
-    ctx.drawImage(enemy[0], 0, 0, size_W, size_H)
-    ctx.drawImage(enemy[1], 100, 0, size_W, size_H)
-    ctx.drawImage(enemy[2], 200, 0, size_W, size_H)
 }
 
 init();
